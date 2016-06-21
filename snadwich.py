4f8c3d53d73e8383a3daf7c367d95c35b72eb0fd
evilaaron11 <aaron.m.a@comcast.net>
2016-06-21 15:46:01 -0400

    Add function to analyze basic math operations

--- snadwich.py
+++ snadwich.py
@@ -1,6 +1,49 @@
+import sys
 
+def performOp(a, b, op):
+    if "ps" == op: # +
+        c = a + b
+    elif "ms" == op: # -
+        c = a - b
+    elif "mx" == op: # *
+        c = a * b
+    elif "dx" == op: # /
+        c = a / b
+    elif "md" == op: # %
+        c = a % b
+    elif "ex" == op: # **
+        c = a ** b
+    else
+        raise ValueError("invalid expression")
+
+def parseTokens(currLine, words):
+    firstInt = True
+    tokens = currLine.split( )
+    for i in tokens:
+        if i.isdigit():
+            if firstInt:
+                firstInt = False
+                j = int(i)
+            else:
+                k = int(i)
+        else:
+            operation = i
+
+    performOp(j, k, operation)
+
+
+def parseLine(currLine):
+    i = 0
+
+    for i in currLine:
+        print(i)
 
 def main():
-    print("Hello world")
+    words =
+    try:
+        #parseLine(sys.argv[1])
+        parseTokens()
+    except IndexError:
+        print("Note enough arguments.")
 
 if __name__ == "__main__": main()
