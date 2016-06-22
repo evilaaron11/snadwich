a387685eb5f16b6846a9d9be21639be8a42143c5
evilaaron11 <aaron.m.a@comcast.net>
2016-06-22 14:48:44 -0400

    Simple edit to snadwich.py

--- snadwich.py
+++ snadwich.py
@@ -1,101 +1,101 @@
 import sys
 
 def printStr(string):
-    quoteFound = False
+   quoteFound = False
     toPrint = ""
 
     for i in string:
-        if quoteFound and i != "\"":
-            toPrint += i
+       if quoteFound and i != "\"":
+          toPrint += i
         if "\"" == i:
-            quoteFound = True
+           quoteFound = True
     print toPrint
 
 def performOp(a, b, op):
-    if "ps" == op: # +
-        c = a + b
-    elif "ms" == op: # -
-        c = a - b
-    elif "mx" == op: # *
-        c = a * b
-    elif "dx" == op: # /
-        c = a / b
-    elif "md" == op: # %
-        c = a % b
-    elif "ex" == op: # **
-        c = a ** b
-    else:
-        raise ValueError("invalid expression")
+   if "ps" == op: # +
+      c = a + b
+   elif "ms" == op: # -
+      c = a - b
+   elif "mx" == op: # *
+      c = a * b
+   elif "dx" == op: # /
+      c = a / b
+   elif "md" == op: # %
+      c = a % b
+   elif "ex" == op: # **
+      c = a ** b
+   else:
+      raise ValueError("invalid expression")
 
-    return c
+   return c
 
 def parseTokens(currLine):
-    firstInt = True
+   firstInt = True
     toPrint = False
     operation = j = k = stringToPrint = None
 
     if currLine[0] == "#": # Comment in code
-        return 0
+       return 0
 
     tokens = currLine.split( )
     for i in tokens:
-        if i.isdigit():
-            if firstInt:
-                firstInt = False
+       if i.isdigit():
+          if firstInt:
+             firstInt = False
                 j = int(i)
             else:
-                k = int(i)
+               k = int(i)
         elif i == "pt":
-            toPrint = True
+           toPrint = True
         elif i == "is":
-            continue
+           continue
         elif '\"' in i:
-            if toPrint:
-                break
-            else:
-                raise SyntaxError("Syntax issue")
+           if toPrint:
+              break
+           else:
+              raise SyntaxError("Syntax issue")
         elif firstInt: # condition for var
-            continue
+           continue
         else:
-            operation = i
+           operation = i
 
 
     # Handle printing
     if toPrint:
-        if j and k:
-            print performOp(j, k, operation)
-        else:
-            printStr(currLine)
+       if j and k:
+          print performOp(j, k, operation)
+       else:
+          printStr(currLine)
             return 0
     a = performOp(j, k, operation)
     return a
 
 
 def parseLine(currLine):
-    i = 0
+   i = 0
 
     for i in currLine:
-        print(i)
+       print(i)
 
 def main():
-    currFile = None
+   currFile = None
     try:
-        #parseLine(sys.argv[1])
+       #parseLine(sys.argv[1])
         #parseTokens()
         currFile = open(sys.argv[1])
     except IndexError:
-        print("Not enough arguments.")
+       print("Not enough arguments.")
         return
     except IOError:
-        print("Invalid filename")
+       print("Invalid filename")
         return
 
     line = currFile.readline()
     while line:
-        try:
-            var = parseTokens(line)
-        except ValueError:
-            print("Invalid operand")
+       try:
+          var = parseTokens(line)
+       except ValueError:
+          print("Invalid operand")
         line = currFile.readline()
     currFile.close()
 
